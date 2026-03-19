612143763538cec81668b2b71f5082b618efcfab
AAdamyan0004 <126516571+AAdamyan0004@users.noreply.github.com>
2026-03-18 20:26:35 -0700

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class NLPRunner {
+  public static void main(String[] args) {
+
+    // Instantiate a MyNLP object ("student-developed class")
+    MyNLP nlp = new MyNLP();
+    String test = "hello  world";
+    nlp.ogSentences.add(test);
+    String revisio = nlp.fixDoubleSpace(test);
+    revisio = nlp.fixPeriod(revisio);
+    revisio = nlp.changeEndingPunctuation(revisio, "?");
+    nlp.correctedSentences.add(revisio);
+    // Calls prompt() method to collect user input (optional)
+    nlp.prompt();
+
+    // Prints the summary/result of the NLP project
+    nlp.printSummary();
+    
+  }
+}
